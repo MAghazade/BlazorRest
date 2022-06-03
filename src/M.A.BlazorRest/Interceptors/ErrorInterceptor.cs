a911d82e9dbdf4d0f3a38053b8a00e7a7b434774
Mohammad Aghazadeh <Aghazadeh1990@gmail.com>
2022-06-04 00:11:57 +0430

    request exception added to error model

--- src/M.A.BlazorRest/Interceptors/ErrorInterceptor.cs
+++ src/M.A.BlazorRest/Interceptors/ErrorInterceptor.cs
@@ -1,8 +1,5 @@
 ﻿using MA.BlazorRest.Src.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 
@@ -10,29 +7,22 @@ namespace MA.BlazorRest.Src.Interceptors
 {
     internal class ErrorInterceptor : IErrorInterceptor
     {
-        public Task IntercepteError(ErrorInterceptorModel? error)
+        public Task InterceptError(ErrorInterceptorModel? error)
         {
-            if (error is null)
+            if (error is null) return Task.CompletedTask;
+
+            if (error.Exception is not null)
             {
-                return Task.CompletedTask;
+                throw new Exception("Network Error!,See Inner Exception For More Details", error.Exception);
             }
 
-            if (error.StatusCode == HttpStatusCode.Unauthorized)
+            switch (error.StatusCode)
             {
-                throw new Exception("Unauthorized Error!,See Inner Exception For More Details", new Exception(error.Content));
+                case HttpStatusCode.Unauthorized: throw new Exception("Unauthorized Error!,See Inner Exception For More Details", new Exception(error.Content));
+                case HttpStatusCode.InternalServerError: throw new Exception("Internal Server Error!,See Inner Exception For More Details", new Exception(error.Content));
+                case HttpStatusCode.BadRequest: throw new Exception("Bad Request,See Inner Exception For More Details", new Exception(error.Content));
+                default: throw new Exception(error.StatusCode.ToString(), new Exception(error.Content));
             }
-
-            if (error.StatusCode == HttpStatusCode.InternalServerError)
-            {
-                throw new Exception("Internal Server Error!,See Inner Exception For More Details", new Exception(error.Content));
-            }
-
-            if (error.StatusCode == HttpStatusCode.BadRequest)
-            {
-                throw new Exception("Bad Request,See Inner Exception For More Details", new Exception(error.Content));
-            }
-
-            return Task.CompletedTask;
         }
     }
 }
--- src/M.A.BlazorRest/Models/ErrorInterceptorModel.cs
+++ src/M.A.BlazorRest/Models/ErrorInterceptorModel.cs
@@ -1,10 +1,12 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace MA.BlazorRest.Src.Models
 {
     public class ErrorInterceptorModel
     {
         public string? Content { get; set; }
-        public HttpStatusCode StatusCode { get; set; }
+        public HttpStatusCode? StatusCode { get; set; }
+        public Exception? Exception { get; set; }
     }
 }
